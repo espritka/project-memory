ec9cce3da3e3e2a79d19726b2e032d0e9ff7dc84
Kasia Lew <espritka@gmail.com>
2018-07-16 20:04:24 -0700

    Trying to add cards to an array of open cards - doesn't work

--- js/app.js
+++ js/app.js
@@ -1,52 +1,68 @@
 /*
- * Create a list that holds all of your cards
- */
+* Create a list that holds all of your cards
+*/
 
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+* set up the event listener for a card. If a card is clicked:
+*  - display the card's symbol (put this functionality in another function that you call from this one)
+*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*  - if the list already has another card, check to see if the two cards match
+*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
 
- const listOfcards = document.querySelectorAll('.card');
- // console.log(listOfcards);   - test if correct list is created
- function displayCardSymbol () {
-   for (let i = 0; i < listOfcards.length; i++) {
-     listOfcards[i].addEventListener('click', function (event) {
-       // console.log("Card was just clicked!");   - test if card is clicked
-       const cardOpen = event.target.classList.toggle('open');
-       const cardShow = event.target.classList.toggle('show');
-     });
-   }
- }
+const deckOfCards = [];     // list that hold all the cards, next step is to shuffle it
+const listOfcards = document.querySelectorAll('.card');
+// console.log(listOfcards);   - test if correct list is created
+let openCards = [];
+let matchedCards = [];
 
- displayCardSymbol();
+function displayCardSymbol () {
+  for (let i = 0; i < listOfcards.length; i++) {
+    listOfcards[i].addEventListener('click', function (event) {
+      // console.log("Card was just clicked!");   - test if card is clicked
+      const cardOpen = event.target.classList.toggle('open');
+      const cardShow = event.target.classList.toggle('show');
+      addToOpenCards();
+    })
+  }
+}
+
+function addToOpenCards() {
+  for (let i = 0; i < listOfcards.length; i++) {
+    listOfcards[i].addEventListener('click', function (event) {
+      const card = event.target;
+      openCards.push();
+      console.log(openCards);
+    })
+  }
+}
+
+
+displayCardSymbol();
